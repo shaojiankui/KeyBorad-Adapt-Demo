fb3127eb02f7447da1bec88befc21f616b11aa12
Jakey <i@skyfox.org>
2014-08-13 08:06:41 +0800

    Initial Commit

---
+++ KeyBorad-Adapt-Demo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  KeyBorad-Adapt-Demo
+//
+//  Created by Jakey on 14-8-13.
+//  Copyright (c) 2014年 www.skyfox.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ KeyBorad-Adapt-DemoTests/KeyBorad_Adapt_DemoTests.m
@@ -0,0 +1,34 @@
+//
+//  KeyBorad_Adapt_DemoTests.m
+//  KeyBorad-Adapt-DemoTests
+//
+//  Created by Jakey on 14-8-13.
+//  Copyright (c) 2014年 www.skyfox.org. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KeyBorad_Adapt_DemoTests : XCTestCase
+
+@end
+
+@implementation KeyBorad_Adapt_DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
